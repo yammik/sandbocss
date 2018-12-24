f55c30033b3f1ce73b9d5a8df70f4a3cc99e30b8
yammik <mkim0514@gmail.com>
2018-12-24 13:05:34 -0500

    add comments in Controls.js

--- src/components/Controls.js
+++ src/components/Controls.js
@@ -4,7 +4,11 @@ import Properties from "./Properties";
 import * as htmlScreenCaptureJs from 'html-screen-capture-js';
 
 class Controls extends Component {
+
   screenCapture = () => {
+    // !!!! COMING SOON !!!! in v2
+    // allow screen capture to go with the generated CSS code
+    // so x below works, but not sure what to do with it yet
     const x = htmlScreenCaptureJs.capture(
       htmlScreenCaptureJs.OutputType.OBJECT,
       window.document,
@@ -13,7 +17,6 @@ class Controls extends Component {
         'imageQualityForDataUrl': 1.0
       }
     );
-    debugger
   }
 
   render() {
